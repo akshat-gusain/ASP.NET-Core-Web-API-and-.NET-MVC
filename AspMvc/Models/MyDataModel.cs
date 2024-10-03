0a5f79fe0addeebe3ee143eb052d49da7ed1a103
Akshat Gusain <106693158+akshat-gusain@users.noreply.github.com>
2024-10-03 12:09:06 +0530

    Add files via upload

---
+++ AspMvc/Models/MyDataModel.cs
@@ -0,0 +1,10 @@
+﻿namespace AspMvc.Models
+{
+    public class MyDataModel
+    {
+        public int Property1 { get; set; }
+        public int Property2 { get; set; }
+        public bool Property3 { get; set; }
+    }
+
+}
