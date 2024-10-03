974177568e1cba06b94495f84b6c465c69a54d3f
Akshat Gusain <106693158+akshat-gusain@users.noreply.github.com>
2024-10-03 12:06:24 +0530

    Add files via upload

---
+++ AspMvc/Controllers/MyDataApiController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using AspMvc.Models;
+
+[ApiController]
+[Route("api/[controller]")]
+public class MyDataApiController : ControllerBase
+{
+    [HttpGet]
+    public IActionResult Get()
+    {
+        var data = new List<MyDataModel>
+        {
+            new MyDataModel { Property1 = 1, Property2 = 100, Property3 = true },  // Change "Example 1" to an int
+            new MyDataModel { Property1 = 2, Property2 = 200, Property3 = false }  // Change "Example 2" to an int
+        };
+        return Ok(data);
+    }
+}
---
+++ AspMvc/Controllers/MyDataController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+using Newtonsoft.Json;
+using AspMvc.Models;
+namespace AspMvc.Controllers
+{
+    public class MyDataController : Controller
+    {
+        private readonly HttpClient _httpClient;
+        public MyDataController(HttpClient httpClient)
+        {
+            _httpClient = httpClient;
+        }
+        // This method calls the API and passes data to the view
+        public async Task<IActionResult> Index()
+        {
+            // Call the API endpoint (replace URL with your actual API endpoint)
+            var response = await _httpClient.GetAsync("https://localhost:44351/api/mydata");
+            response.EnsureSuccessStatusCode();
+            // Deserialize the JSON response into a list of objects
+            var jsonResponse = await response.Content.ReadAsStringAsync();
+            var data = JsonConvert.DeserializeObject<List<MyDataModel>>(jsonResponse);
+            // Pass the data to the view
+            return View(data);
+        }
+    }
+}
+
+
+
+
+
